81989349eba7a1a213e1b8b659045bba0061a30e
Mike Carifio <mike@carif.io>
2016-05-13 09:41:22 -0400

    count

---
+++ count2.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+
+for my $i (0..9) {
+    print($i, "\n");
+}
---
+++ count2a.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/env perl
+
+# ARGV, the command line arguments
+for my $i (0..${ARGV[0]}) {
+    print($i, "\n");
+}
